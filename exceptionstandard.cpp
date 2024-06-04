c0eb0b1095d043fb1a27daf60f51d89b8b422470
mahasiswa229 <161399711+mahasiswa229@users.noreply.github.com>
2024-06-04 16:11:38 +0800

    catch n return

--- exceptionstandard.cpp
+++ exceptionstandard.cpp
@@ -11,5 +11,10 @@ int main ()
        array<int, 3> data = {3, 5, 7};
        cout << data.at(5) << endl;
     }
-    
+    catch(exception& e)
+    {
+        cout << e.what()<< endl;
+    }
+    cout << "Baris Program yang Terakhir" << endl;
+    return 0;
 }
