f8ab695200c3e7578b67a42dc11f18e05d80a1d7
mahasiswa229 <161399711+mahasiswa229@users.noreply.github.com>
2024-06-04 16:11:23 +0800

    try

--- exceptionstandard.cpp
+++ exceptionstandard.cpp
@@ -6,5 +6,10 @@ using namespace std;
 int main ()
 {
     cout << "Awal Program "  << endl;
+    try
+    {
+       array<int, 3> data = {3, 5, 7};
+       cout << data.at(5) << endl;
+    }
     
 }
