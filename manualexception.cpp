22d204b7039efde77e5335b5312e7f6297fb801c
mahasiswa229 <161399711+mahasiswa229@users.noreply.github.com>
2024-06-04 16:20:45 +0800

    manual exception

--- manualexception.cpp
+++ manualexception.cpp
@@ -3,5 +3,19 @@ using namespace std;
 
 int main()
 {
-    
+    try
+    {
+        cout << "Selamat Belajar di Prodi TI UMY" << endl;
+        throw 0.5;
+        cout << "Pernyataan tidak akan dieksekusi" << endl;
+    }
+    catch(int a)
+    {
+        cout << "Pengecualian tidak akan dieksekusi" << endl;
+    }
+    catch(...)
+    {
+        cout << "default Pengecualian dieksekusi" << endl;
+    }
+    return 0;
 }
